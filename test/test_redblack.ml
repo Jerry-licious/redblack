688e3abf4e9f510547a5a1564f40f1c7ef344f55
Zoe Wang <wang.zoe2005@gmail.com>
2024-11-18 17:35:37 -0500

    flatten and treemap tests

--- test/test_redblack.ml
+++ test/test_redblack.ml
@@ -15,6 +15,61 @@ let delete_tests = "Random Deletions" >::: [
   "Deletion: Search" >:: (fun _ -> random_deletes_lookups test_size);
 ];;
 
+let max_size = 1000
+
+let random_tree () =
+  let size = Random.int max_size in
+  let values = List.init size (fun _ -> Random.int 1000) in
+  make_rb cmp values
+
+let flatten_tests = "Flatten" >::: [
+  "Flatten: Random Tree" >:: (fun _ ->
+    let tree = random_tree() in 
+    let flattened = flatten tree in 
+    let sorted = List.sort compare flattened in 
+    assert_equal sorted flattened
+    );
+
+  "Flatten: Empty Tree" >:: (fun _ ->
+    let tree = Leaf in
+    let flattened = flatten tree in 
+    assert_equal [] flattened
+    );
+
+  "Flatten: Single Node Tree" >:: (fun _ ->
+    let tree = Node (Black, 10, Leaf, Leaf) in 
+    let flattened = flatten tree in 
+    assert_equal [10] flattened
+    );
+
+];;
+
+let treemap_tests = "Treemap" >::: [
+  "Treemap: Random Tree" >:: (fun _ ->
+    let tree = random_tree () in 
+    let squared = treemap (fun x -> x * x ) tree in 
+    let sorted = List.sort compare squared in 
+    assert_equal sorted squared
+    );
+
+  "Treemap: Empty Tree" >:: (fun _ ->
+    let tree = Leaf in 
+    let result = treemap (fun x -> x * x) tree in 
+    assert_equal [] result
+    );
+  
+    "Treemap: Single Node Tree" >:: (fun _ ->
+      let tree = Node (Black, 10, Leaf, Leaf) in 
+      let squared = treemap (fun x -> x * x) tree in 
+      assert_equal [100] squared
+      );
+];;
+
+
+
+
 print_string "Running tests";;
 let _ = run_test_tt_main insert_tests;;
 let _ = run_test_tt_main delete_tests;;
+let _ = run_test_tt_main flatten_tests;;
+let _ = run_test_tt_main treemap_tests;;
